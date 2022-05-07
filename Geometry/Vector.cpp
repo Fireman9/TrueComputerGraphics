e7dbc622c2325c890af9dc21ab8f2739f38579a9
fireman9 <nazariystr@gmail.com>
2022-05-07 17:43:06 +0300

    added Vector class

---
+++ Geometry/Vector.cpp
@@ -0,0 +1,68 @@
+#include <cmath>
+#include "Vector.h"
+
+Vector::Vector() : mX(0), mY(0), mZ(0) {}
+
+Vector::Vector(Point &start, Point &end) {
+	this->mX = end.x() - start.x();
+	this->mY = end.y() - start.y();
+	this->mZ = end.z() - start.z();
+}
+
+Vector::Vector(double startX, double startY, double startZ, double endX, double endY, double endZ) {
+	this->mX = startX - endX;
+	this->mY = startY - endY;
+	this->mZ = startZ - endZ;
+}
+
+Vector::Vector(double x, double y, double z) : mX(x), mY(y), mZ(z) {}
+
+void Vector::setCoordinates(double x, double y, double z) {
+	setX(x), setY(y), setZ(z);
+}
+
+double Vector::length() const {
+	return sqrt(x() * x() + y() * y() + z() * z());
+}
+
+Vector Vector::operator+(Vector &v) const {
+	return {this->x() + v.x(), this->y() + v.y(), this->z() + v.z()};
+}
+
+Vector Vector::operator-(Vector &v) const {
+	return {this->x() - v.x(), this->y() - v.y(), this->z() - v.z()};
+}
+
+double Vector::x() const {
+	return mX;
+}
+
+double Vector::y() const {
+	return mY;
+}
+
+double Vector::z() const {
+	return mZ;
+}
+
+void Vector::setX(double x) {
+	this->mX = x;
+}
+
+void Vector::setY(double y) {
+	this->mY = y;
+}
+
+void Vector::setZ(double z) {
+	this->mZ = z;
+}
+
+double Vector::dotProduct(Vector &v1, Vector &v2) {
+	return v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z();
+}
+
+Vector Vector::crossProduct(Vector &v1, Vector &v2) {
+	return {v1.y() * v2.z() - v1.z() * v2.y(),
+			v1.z() * v2.x() - v1.x() * v2.z(),
+			v1.x() * v2.y() - v1.y() * v2.x()};
+}
---
+++ Geometry/Vector.h
@@ -0,0 +1,45 @@
+#ifndef TRUECOMPUTERGRAPHICS_GEOMETRY_VECTOR_H_
+#define TRUECOMPUTERGRAPHICS_GEOMETRY_VECTOR_H_
+
+#include "Point.h"
+
+class Vector {
+public:
+	Vector();
+
+	Vector(Point &start, Point &end);
+
+	Vector(double startX, double startY, double startZ, double endX, double endY, double endZ);
+
+	Vector(double x, double y, double z);
+
+	void setCoordinates(double x, double y, double z);
+
+	double length() const;
+
+	Vector operator+(Vector &v) const;
+
+	Vector operator-(Vector &v) const;
+
+	double x() const;
+
+	double y() const;
+
+	double z() const;
+
+	void setX(double x);
+
+	void setY(double y);
+
+	void setZ(double z);
+
+	static double dotProduct(Vector &v1, Vector &v2);
+
+	static Vector crossProduct(Vector &v1, Vector &v2);
+
+private:
+	double mX, mY, mZ;
+
+};
+
+#endif //TRUECOMPUTERGRAPHICS_GEOMETRY_VECTOR_H_
