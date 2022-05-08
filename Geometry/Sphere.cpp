f9a5f55e1d5bc35e3c2feaf6bc5f2ccb53bb669e
fireman9 <nazariystr@gmail.com>
2022-05-08 15:32:20 +0300

    added base Sphere class

---
+++ Geometry/Sphere.cpp
@@ -0,0 +1,21 @@
+#include "Sphere.h"
+
+Sphere::Sphere(double radius, Point &center) : mRadius(radius), mCenter(center) {}
+
+Sphere::Sphere(double radius, double x, double y, double z) : mRadius(radius), mCenter(Point(x, y, z)) {}
+
+double Sphere::radius() const {
+	return this->mRadius;
+}
+
+Point Sphere::center() const {
+	return this->mCenter;
+}
+
+void Sphere::setRadius(double radius) {
+	this->mRadius = radius;
+}
+
+void Sphere::setCenter(const Point &center) {
+	this->mCenter = center;
+}
---
+++ Geometry/Sphere.h
@@ -0,0 +1,26 @@
+#ifndef TRUECOMPUTERGRAPHICS_GEOMETRY_SPHERE_H_
+#define TRUECOMPUTERGRAPHICS_GEOMETRY_SPHERE_H_
+
+#include "Point.h"
+
+class Sphere {
+public:
+	Sphere(double radius, Point &center);
+
+	Sphere(double radius, double x, double y, double z);
+
+	double radius() const;
+
+	Point center() const;
+
+	void setRadius(double radius);
+
+	void setCenter(const Point &center);
+
+private:
+	double mRadius;
+	Point mCenter;
+
+};
+
+#endif //TRUECOMPUTERGRAPHICS_GEOMETRY_SPHERE_H_
