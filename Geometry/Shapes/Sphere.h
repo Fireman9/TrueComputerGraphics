75719ad40fcb935486133d5775467cde3620c713
fireman9 <nazariystr@gmail.com>
2022-05-09 13:41:13 +0300

    added finding ray interception points to Sphere

--- Geometry/Shapes/Sphere.h
+++ Geometry/Shapes/Sphere.h
@@ -2,12 +2,28 @@
 #define TRUECOMPUTERGRAPHICS_GEOMETRY_SPHERE_H_
 
 #include "../Point.h"
+#include "../Ray.h"
 
 class Sphere {
+
 public:
+	enum Intersections {
+		NoIntersection,
+		OnePointIntersection,
+		TwoPointIntersection
+	};
+
 	Sphere(double radius, Point center);
 
 	Sphere(double radius, double x, double y, double z);
+
+	void getIntersectionVars(double &a, double &b, double &c, double &delta, Ray ray) const;
+
+	Intersections isRayIntersection(Ray ray) const;
+
+	Point getOnePointRayIntersection(Ray ray) const;
+
+	std::pair<Point, Point> getTwoPointRayIntersection(Ray ray) const;
 
 	double radius() const;
 
