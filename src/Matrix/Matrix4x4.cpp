c2dfd623e462cd3ecdd63b32bb765a2edb84c01f
Alois <sonya.shu2107@gmail.com>
2022-05-26 01:14:24 +0300

    Add basic matrix class with translation, scaling, shear and rotation

---
+++ src/Matrix/Matrix4x4.cpp
@@ -0,0 +1,59 @@
+#include "Matrix4x4.h"
+
+Matrix4x Matrix4x::rotateX(double a) {
+	Matrix4x m({ 
+		{1,0,0,0},
+		{0,cos(a),-sin(a),0},
+		{0,sin(a),cos(a),0},
+		{0,0,0,1} });
+	return m;
+};
+Matrix4x Matrix4x::rotateY(double a) {
+	Matrix4x m({
+		{cos(a),0,sin(a),0},
+		{0,1,0,0},
+		{-sin(a),0,cos(a),0},
+		{0,0,0,1} });
+	return m;
+};
+Matrix4x Matrix4x::rotateZ(double a) {
+	Matrix4x m({
+		{cos(a),-sin(a),0,0},
+		{sin(a),cos(a),0,0},
+		{0,0,1,0},
+		{0,0,0,1} });
+	return m;
+};
+Matrix4x Matrix4x::scale(double x, double y, double z) {
+	Matrix4x m({
+		{x,0,0,0},
+		{0,y,0,0},
+		{0,0,z,0},
+		{0,0,0,1} });
+	return m;
+};
+Matrix4x Matrix4x::shear(double xy, double xz, double yx, double yz, double zx, double zy) {
+	Matrix4x m({
+		{1,yx,zx,0},
+		{xy,1,zy,0},
+		{xz,yz,1,0},
+		{0,0,0,1} });
+	return m;
+};
+Matrix4x Matrix4x::transpose(double x, double y, double z) {
+	Matrix4x m({
+		{1,0,0,0},
+		{0,1,0,0},
+		{0,0,1,0},
+		{x,y,z,1} });
+	return m;
+};
+	
+Matrix4x::Matrix4x() { setMatrix({}); };
+Matrix4x::Matrix4x(vector<vector<double>> m) { setMatrix(m); };
+void Matrix4x::setMatrix(vector<vector<double>> m) {
+	if (m.empty()) {
+		m = { {1,0,0,0},{0,1,0,0},{0,0,1,0},{0,0,0,1} };
+	}
+	this->matrix = m;
+};
---
+++ src/Matrix/Matrix4x4.h
@@ -0,0 +1,18 @@
+#include <vector>
+using namespace std;
+
+
+class Matrix4x {
+public:
+	static Matrix4x rotateX(double a);
+	static Matrix4x rotateY(double a);
+	static Matrix4x rotateZ(double a);
+	static Matrix4x scale(double x, double y, double z);
+	static Matrix4x shear(double xy, double xz, double yx, double yz, double zx, double zy);
+	static Matrix4x transpose(double x, double y, double z);
+private:
+	Matrix4x();
+	Matrix4x(vector<vector<double>> m);
+	vector<vector<double>> matrix;
+	void setMatrix(vector<vector<double>> m);
+};
