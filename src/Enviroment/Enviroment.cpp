ce49550d6b199a4380039caec4becd68a1a39b42
fireman9 <nazariystr@gmail.com>
2022-06-25 03:37:55 +0300

    set 3 mirror spheres in room

--- src/Enviroment/Enviroment.cpp
+++ src/Enviroment/Enviroment.cpp
@@ -4,7 +4,7 @@ Enviroment::Enviroment(vector<string> mArg) {
 	string tmp;
 	bool find = false;
 	setArg(mArg);
-	tmp = findKey("--source=",&find);
+	tmp = findKey("--source=", &find);
 	if (find) setInput(tmp);
 	find = false;
 	tmp = findKey("--output=", &find);
@@ -23,20 +23,21 @@ Enviroment::Enviroment(vector<string> mArg) {
 	else setWidth(200);
 }
 
-OBJReader  Enviroment::readTriangles() {
+OBJReader Enviroment::readTriangles() {
 	OBJReader objReader(input());
 	objReader.read();
 	return objReader;
 }
 
+vector<Triangle> Enviroment::transformTriangles(Matrix4x4 matrix, vector<Triangle> triangles, Point start) {
+	for (auto &triangle : triangles) {
+		triangle = triangle.transform(matrix, start);
+	}
+	return triangles;
+}
+
 Scene Enviroment::prepare() {
-
-	//  OBJReader objReader = readTriangles();
-
 	Screen screen(width(), height());
-	//vector<Light*> light = { new DotLight(Point(2,2,2),Color::red()),new DotLight(Point(-2,-2,2),Color::green()), new DotLight(Point(0,3,0), Color::blue())};
-	//vector<Light*> light = { new Light(Color::green(),0.005), new Light(Color::red(),0.01),new DotLight(Point(2,2,2),Color(255,0,255,255), 0.5) };
-	//vector<Light*> light = { new DotLight(Point(2,2,2),Color(255,0,255,255), 0.7) };
 
 	auto l1 = std::make_shared<DirectLight>(Vector(2, -4, -3), Color::white(), 0.4);
 	auto l3 = std::make_shared<DotLight>(Point(-2, -4, -3), Color::white(), 0.7);
@@ -47,24 +48,17 @@ Scene Enviroment::prepare() {
 	light.push_back(l3);
 	light.push_back(l4);
 	Scene scene(screen, light, Point(0, 0, -10), 100);
-	//Scene scene(screen, light, Point(0, 0, -5), 250);
 
-	//  Matrix4x4 matrix = Matrix4x4::rotateX(-1.57) * Matrix4x4::rotateY(-0.735);
-	//Matrix4x4 matrix = Matrix4x4::rotateX(-4.0) * Matrix4x4::rotateY(-1.235) * Matrix4x4::transpose(0, 0.3, 0);
-	//Matrix4x4 matrix = Matrix4x4::rotateX(0) * Matrix4x4::rotateY(0);
-
-	//  triangles = transformTriangles(matrix, objReader, Point(0,0,0));
-
-	//  scene.setTriangles(triangles);
 	Plane plane1({0, 1, 0}, 9);
 	Plane plane2({0, 1, 0}, -3);
 	Plane plane3({1, 0, 0}, -10);
 	Plane plane4({1, 0, 0}, 10);
 	Plane plane5({0, 0, 1}, -10);
-	Sphere sphere1(1, {0, 2, 0});
-	Sphere sphere2(2, {1, -10, 10}, Sphere::Mirror);
+	Sphere sphere1(1, {0, 2, 0}, Shape::Material::Mirror);
+	Sphere sphere2(2, {1, -10, 10}, Shape::Material::Mirror);
+	Sphere sphere3(1, {3, 2, 0}, Shape::Material::Mirror);
 
-	scene.setSpheres({sphere1, sphere2});
+	scene.setSpheres({sphere1, sphere2, sphere3});
 	scene.setPlanes({plane1, plane2, plane3, plane4, plane5});
 
 	return scene;
@@ -75,38 +69,47 @@ void Enviroment::writeTofile(Scene scene) {
 	if (isTesting()) {
 		this->tStart = clock();
 		scene.renderScene();
-		printf("Time taken for simple rendering: %.5fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+		printf("Time taken for simple rendering: %.5fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
 		const string out = "TEST_RENDER.ppm";
 		ppmWriter.setFilepath(out);
 		scene.writeRenderToPPM(ppmWriter);
 	}
 	ppmWriter.setFilepath(output());
 	this->tStart = clock();
-	Node* n = Node::createNode(triangles);
+	Node *n = Node::createNode(triangles);
 	scene.setTree(Node::createNode(triangles));
-	printf("Time taken for tree creating: %.5fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+	printf("Time taken for tree creating: %.5fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
 
 	this->tStart = clock();
 	scene.renderSceneTree();
-	printf("Time taken for tree rendering: %.5fs\n", (double)(clock() - tStart) / CLOCKS_PER_SEC);
+	printf("Time taken for tree rendering: %.5fs\n", (double) (clock() - tStart) / CLOCKS_PER_SEC);
 
 	scene.writeRenderToPPM(ppmWriter);
 }
 
-void Enviroment::setArg(vector<string> mArg) {this->arg = mArg;}
+void Enviroment::setArg(vector<string> mArg) { this->arg = mArg; }
+
 void Enviroment::setInput(string inp) { this->inputfileName = inp; }
+
 void Enviroment::setOut(string out) { this->outPutFileName = out; }
+
 void Enviroment::setHeight(int px) { this->heightS = px; }
+
 void Enviroment::setWidth(int px) { this->widthS = px; }
+
 void Enviroment::setIsTesting(bool b) { this->isTestImg = b; }
 
 string Enviroment::input() { return this->inputfileName; }
+
 string Enviroment::output() { return this->outPutFileName; }
+
 int Enviroment::height() { return this->heightS; }
+
 int Enviroment::width() { return this->widthS; }
+
 bool Enviroment::isTesting() { return this->isTestImg; }
 
-string Enviroment::findKey(string ex, bool* find) {
+string Enviroment::findKey(string ex, bool *find) {
 	string tmp;
 	for (auto argument : arg) {
 		if (argument.find(ex) != std::string::npos) {
