7534bc4eab25af5225d9ace3c798352767bf8e46
fireman9 <nazariystr@gmail.com>
2022-06-26 23:37:47 +0300

    add Texture class

--- src/Render/Texture.cpp
+++ src/Render/Texture.cpp
@@ -1,12 +1,28 @@
 #include "Texture.h"
 
+Texture::Texture() = default;
+
 Texture::Texture(Color color) : mColor(color) {}
 
-Color Texture::getColor() {
+Texture::Texture(vector<vector<Color>> image) : mImage(std::move(image)) {}
+
+Color Texture::getColor(Point &localHitPoint, Mapper *mapper) {
+	if (mImage.empty()) return mColor;
+
+	// pass mapper as parameter
+	// call mapper
 	// image processing here
-	return mColor;
+
+}
+
+vector<vector<Color>> Texture::getImage() {
+	return mImage;
 }
 
 void Texture::setColor(Color color) {
 	mColor = color;
+}
+
+void Texture::setImage(vector<vector<Color>> image) {
+	mImage = std::move(image);
 }
--- src/Render/Texture.h
+++ src/Render/Texture.h
@@ -1,19 +1,29 @@
 #ifndef TRUECOMPUTERGRAPHICS_SRC_RENDER_TEXTURE_H_
 #define TRUECOMPUTERGRAPHICS_SRC_RENDER_TEXTURE_H_
 
+#include <vector>
+#include <utility>
+
 #include "Color.h"
+#include "Mappers/Mapper.h"
+
+using std::vector;
 
 class Texture {
 public:
+	Texture();
 	Texture(Color color);
+	Texture(vector<vector<Color>> image);
 
-	Color getColor();
+	Color getColor(Point &localHitPoint, Mapper *mapper);
+	vector<vector<Color>> getImage();
 
 	void setColor(Color color);
+	void setImage(vector<vector<Color>> image);
 
 private:
 	Color mColor;
-//	Image mImage;
+	vector<vector<Color>> mImage;
 };
 
 #endif //TRUECOMPUTERGRAPHICS_SRC_RENDER_TEXTURE_H_
